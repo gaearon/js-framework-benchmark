d237a6813c27f6326565ebca43834c39b4d3e484
Dan Abramov <dan.abramov@gmail.com>
2016-08-10 14:49:35 +0100

    15.3.0

---
+++ react-v15.3.0/webpack.config.dev.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var webpack = require('webpack');
+
+var cache = {};
+var loaders = [
+	{
+		test: /\.jsx$/,
+		loader: 'babel-loader'
+	},
+	{
+		test: /\.es6\.js$/,
+		loader: 'babel-loader'
+	},
+	{
+		test: /\.css$/,
+		loader: 'style-loader!css-loader'
+	}
+];
+var extensions = [
+	'', '.js', '.jsx', '.es6.js'
+];
+
+module.exports = [{
+	module: {
+		loaders: loaders
+	},
+	entry: {
+		main: './src/main.es6.js',
+	},
+	output: {
+		path: './dist',
+		filename: '[name].js'
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': '"development"'
+		})
+	],
+	resolve: {
+		extensions: extensions
+	}
+}];
